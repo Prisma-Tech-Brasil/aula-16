d5e330ba4f532a330d121d3e1316304fd81db32f
wallacevieira <wallaceofc@hotmail.com>
2024-07-09 14:54:46 -0300

    first commit

---
+++ codigos/true-false/script.js
@@ -0,0 +1,7 @@
+var queijo = "Cheddar";
+
+if (queijo) {
+  console.log("Oba! Queijo disponível para fazer torrada com queijo.");
+} else {
+  console.log("Sem torrada com queijo para você hoje.");
+}
